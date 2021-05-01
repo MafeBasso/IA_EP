95771b6a36109aff42f8941c5a3cf4f364149331
agspf <agspf94@gmail.com>
2021-05-01 08:34:51 -0300

    [agspf94] Exercício 3

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas
+import math
+import numpy as np
 
 def readDataset():
     return pandas.read_csv("Dataset_1.csv")
@@ -55,6 +57,24 @@ def exploratoryAnalysis(dataset):
             print()
     print(separator)
 
+def knn(dataset, query, k=1):
+    if k >= len(dataset):
+        k = len(dataset) - 1
+    columns = dataset.columns.size
+    distances = dataset.apply(lambda row: function(query, row, columns), axis=1)
+    minimal_distances = distances.sort_values()[0:k]
+    classes = list()
+    for i in range(minimal_distances.size):
+        classes.append(dataset.loc[minimal_distances.index[i]][columns - 1])
+    unique_classes = np.unique(classes)
+    votes_per_class = [0] * unique_classes.size
+    for i in range(unique_classes.size):
+        votes_per_class[i] = classes.count(unique_classes[i])
+    return unique_classes[votes_per_class.index(max(votes_per_class))]
+
+def function(query, row, classId):
+    return math.sqrt(sum((query - row[0:(classId - 1)]) ** 2))
+
 # Lê e imprime o dataset original
 dataset = readDataset()
 printDataset(dataset, "Dataset")
@@ -68,3 +88,7 @@ printDataset(dataset, "Dataset Normalizado")
 
 # Mostra a análise exploratória dos dados
 exploratoryAnalysis(dataset)
+
+query = [78.0, 9.81147629156, 9.75889547269, 9.84941095639, 0.0146255073331, 0.00165077522136, 0.0,
+         0.003533082558530001, 0.000736963641413, 0.0, 0.0, 0.0, 0.0]
+print(knn(dataset, query, 10))

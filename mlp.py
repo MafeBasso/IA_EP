fcf9e93da8175ca3ae3528cb23f16ca972dfb111
agspf <agspf94@gmail.com>
2021-05-22 14:47:12 -0300

    [agspf94] MLP finished

--- mlp.py
+++ mlp.py
@@ -5,10 +5,22 @@ import pickle
 
 # Leitura do dataset
 def read_dataset(dataset):
-    return pandas.read_csv(dataset)
+    dataset = pandas.read_csv(dataset)
+    dataset = dataset.drop([
+        "accelerometer#min",
+        "accelerometer#max",
+        "accelerometer#std",
+        "gyroscope#min",
+        "gyroscope#max",
+        "gyroscope#std",
+        "sound#min",
+        "sound#max",
+        "sound#std"],
+        axis=1)
+    return dataset
 
 
-def pre_processing():
+def pre_processing(dataset):
     # Como apenas a coluna target possui strings, ela recebe um tratamento diferente
     for column in dataset.columns:
         if column == "still" or column == "car" or column == "train" or column == "bus" or column == "walking":
@@ -27,28 +39,30 @@ def pre_processing():
 def normalize(dataset, classes):
     # Removemos a coluna target para fazer a normalização porque ela possui apenas strings
     # dataset_values = dataset[:len(dataset) - classes]
-    dataset_attributes = dataset.drop(["still", "car", "train", "bus", "walking"], axis = 1)
+    dataset_attributes = dataset.drop(["still", "car", "train", "bus", "walking"], axis=1)
     dataset_classes = dataset.drop([
         "time",
         "accelerometer#mean",
-        "accelerometer#min",
-        "accelerometer#max",
-        "accelerometer#std",
         "gyroscope#mean",
-        "gyroscope#min",
-        "gyroscope#max",
-        "gyroscope#std",
-        "sound#mean",
-        "sound#min",
-        "sound#max",
-        "sound#std"],
-        axis = 1)
+        "sound#mean"
+        ],
+        axis=1)
     normalized = ((dataset_attributes - dataset_attributes.min()) / (dataset_attributes.max() - dataset_attributes.min()))
     normalized = numpy.array(normalized)
     dataset_classes = numpy.array(dataset_classes)
-    dataset = numpy.append(normalized, dataset_classes, axis = 1)
-    print(normalized)
-    return normalized
+    dataset = numpy.append(normalized, dataset_classes, axis=1)
+    dataset = pandas.DataFrame(dataset, columns=[
+        "time",
+        "accelerometer#mean",
+        "gyroscope#mean",
+        "sound#mean",
+        "still",
+        "car",
+        "train",
+        "bus",
+        "walking"
+    ])
+    return dataset
 
 
 # Cálculo de fnet
@@ -94,7 +108,7 @@ class Model:
         print()
 
 
-def create_model(input_layer = 2, hidden_layer = 2, output_layer = 1, activation_function = fnet, d_activation_function = d_fnet):
+def create_model(input_layer, hidden_layer, output_layer, activation_function = fnet, d_activation_function = d_fnet):
     hidden_weights = numpy.random.uniform(-0.5, 0.5, (hidden_layer, input_layer + 1))
     output_weights = numpy.random.uniform(-0.5, 0.5, (output_layer, hidden_layer + 1))
 
@@ -122,8 +136,11 @@ class ModelForward:
         print("f_net_o_p = \n" + str(self.f_net_o_p))
         print()
 
-    def print_result(self):
-        print("Result = 0") if (self.f_net_o_p[0] < 0.5) else print("Result = 1")
+    def get_result(self):
+        selected = self.f_net_o_p.index(max(self.f_net_o_p))
+        print(selected)
+        classes = ["still", "car", "train", "bus", "walking"]
+        return classes[selected]
 
 
 def test_model(model, p):
@@ -154,7 +171,13 @@ def test_model(model, p):
     return ModelForward(net_h_p, f_net_h_p, net_o_p, f_net_o_p)
 
 
-def backpropagation(model, dataset, eta = 0.1, threshold = 1e-1):
+def backpropagation(model, dataset, eta = 0.1, threshold = 0.65):
+    print("Initial Hidden Weights = ")
+    print(model.hidden_weights)
+    print("Initial Output Weights = ")
+    print(model.output_weights)
+    print("MLP is running. Please wait...")
+
     squared_error = 2 * threshold
     cycles = 0
 
@@ -162,10 +185,10 @@ def backpropagation(model, dataset, eta = 0.1, threshold = 1e-1):
         squared_error = 0
 
         for i in range(len(dataset)):
-            x_p = dataset.to_numpy()[i][:model.input_layer].tolist()
-            y_p = dataset.to_numpy()[i][model.input_layer:dataset.columns.size]
+            x_p = dataset.to_numpy()[i][:model.input_layer].tolist() # lista dos atributos
+            y_p = dataset.to_numpy()[i][model.input_layer:dataset.columns.size] # vetor das classes
 
-            tested_model = test_model(model, x_p)
+            tested_model = test_model(model, x_p) # model forwarded com as nets
             obtained_value_p = tested_model.f_net_o_p
 
             # Error calculation
@@ -178,25 +201,31 @@ def backpropagation(model, dataset, eta = 0.1, threshold = 1e-1):
                 delta_o_p[j] = error[j] * model.d_activation_function(tested_model.f_net_o_p[j])
 
             # Training hidden layer
-            w_o_k = [0] * len(tested_model.f_net_o_p)
-            delta_h_p = [0] * len(tested_model.f_net_o_p)
+            w_o_k = [0] * len(tested_model.f_net_o_p) # matriz de listas dos pesos da output
+            delta_h_p = [0] * len(tested_model.f_net_o_p) # lista de numpy array
             for j in range(len(delta_h_p)):
                 w_o_k[j] = model.output_weights[j][:model.hidden_layer].tolist()
                 delta_h_p[j] = delta_o_p[j] * numpy.array(w_o_k[j]) * model.d_activation_function(tested_model.f_net_h_p[j])
 
             # Training
             for j in range(len(delta_o_p)):
-                model.output_weights[j] += eta * (delta_o_p[j] * numpy.array(tested_model.f_net_h_p))
+                model.output_weights[j] += eta * (delta_o_p[j] * numpy.array(tested_model.f_net_h_p)) # matriz com pesos da output
 
             for j in range(len(delta_h_p)):
-                model.hidden_weights[j] += eta * numpy.multiply(numpy.array(delta_h_p).T, x_p)[j]
+                model.hidden_weights[j] += eta * numpy.multiply(numpy.array(delta_h_p), x_p)[j]
 
         squared_error /= len(dataset)
         cycles += 1
-        if (cycles % 10000 == 0):
-            print(squared_error)
+        # if (cycles % 10 == 0):
+        #     print(squared_error)
 
-    print()
+    print("MLP has finished running.")
+    print("Final Hidden Weights = ")
+    print(model.hidden_weights)
+    print("Final Output Weights = ")
+    print(model.output_weights)
+    print("Cycles = " + str(cycles))
+
     model.cycles = cycles
     save_trained_model(model)
     return model
@@ -207,12 +236,12 @@ def save_trained_model(model):
         pickle.dump(model, model_file)
 
 
-def create(input_layer = 2, hidden_layer = 2, output_layer = 1, activation_function = fnet, d_activation_function = d_fnet):
+def create(input_layer, hidden_layer, output_layer, activation_function = fnet, d_activation_function = d_fnet):
     model = create_model(input_layer, hidden_layer, output_layer, activation_function, d_activation_function)
     return backpropagation(model, dataset)
 
 
-def load_trained_model(recreate_model = False, input_layer = 2, hidden_layer = 2, output_layer = 1, activation_function = fnet, d_activation_function = d_fnet):
+def load_trained_model(input_layer, hidden_layer, output_layer, recreate_model = False, activation_function = fnet, d_activation_function = d_fnet):
     if recreate_model:
         return create(input_layer, hidden_layer, output_layer, activation_function, d_activation_function)
     else:
@@ -233,12 +262,20 @@ def get_training_and_test(dataset, percentage, lines_removed = 0):
     return dataset[:x], dataset[x:len(dataset)]
 
 
-def calculate_accuracy(model, test_dataset):
+def get_result(fnets, tests):
+    tests = tests.tolist()
+    if fnets.index(max(fnets)) == tests.index(max(tests)):
+        return True
+    return False
+
+
+def calculate_accuracy(model, test_dataset, classes):
     hits = 0
     errors = 0
-    for i in range(len(test_dataset)):
-        result = test_model(model, test_dataset[i][:len(test_dataset) - 1])
-        if result == test_dataset[i][len(dataset) - 1:len(dataset)]:
+    for i in range(test_dataset.first_valid_index(), test_dataset.last_valid_index() + 1):
+        test_tuple = numpy.array(test_dataset.loc[i])
+        result = test_model(model, test_tuple[:test_dataset.first_valid_index() + len(test_dataset) - classes].tolist())
+        if get_result(result.f_net_o_p, test_tuple[classes - 1:]):
             hits += 1
         else:
             errors += 1
@@ -250,16 +287,16 @@ def calculate_accuracy(model, test_dataset):
 dataset = read_dataset(dataset = "Dataset_TMD_MLP.csv")
 
 # Faz o pré-processamento dos dados
-dataset = pre_processing()
+dataset = pre_processing(dataset)
 
 # Normaliza o dataset usando a re-escala linear
 dataset = normalize(dataset, classes = 5)
 
 # Obtém o dataset de treino e o dataset de teste com base na porcentagem escolhida
-training_dataset, test_dataset = get_training_and_test(dataset = dataset, percentage = 0.7)
+training_dataset, test_dataset = get_training_and_test(dataset = dataset, percentage = 0.7, lines_removed = 5884)
 
 # Carrega o modelo já treinado, ou treina um novo caso ainda não exista um modelo
-model = load_trained_model(recreate_model = False, input_layer = 13, hidden_layer = 3, output_layer = 5)
+model = load_trained_model(input_layer = 4, hidden_layer = 5, output_layer = 5, recreate_model = False)
 
 # Valida o dataset de testes
-calculate_accuracy(model, test_dataset)
+calculate_accuracy(model, test_dataset, classes = 5)
